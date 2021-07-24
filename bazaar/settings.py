0b258ada4439ba5e0533d67166df4a43fe65a13b
RITIK BHOLA <ritikbhola321456@gmail.com>
2021-07-24 12:46:46 +0530

    Update settings.py

--- bazaar/settings.py
+++ bazaar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-++ek_a!yvs65&+4kpio%3_@@s+$a9@=w4_cqvg-rxhxks-590!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['bazaars.herokuapp.com']
 
